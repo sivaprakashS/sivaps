703d2a1e6ce93be92b0051140a82c66ad20315eb
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-01-31 02:39:16 -0800

    sivaps.c

---
+++ sivaps.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+printf("\nenter the number");
+scanf("%d",&n);
+if(n>0)
+printf("\n the number is positive");
+else if(n<0)
+printf("\nthe number is negative");
+else if(n=0)
+printf("\n the number is zero");
+else
+printf("\nit is not a number");
+getch()
+}
